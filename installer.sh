fd84f11b23f43df0b8c049193e0148729003930b
Daniel Hiller <46579393+daniel-hiller@users.noreply.github.com>
2024-01-29 16:34:06 +0100

    Update wireguard-ui to v0.6.2

--- installer.sh
+++ installer.sh
@@ -123,11 +123,11 @@ function wgui_installer {
     mkdir /opt/wireguard-ui
     arch=$(uname -m)
     if [[ $arch == x86_64* ]]; then
-        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.4.0/wireguard-ui-v0.4.0-linux-amd64.tar.gz  -O /opt/wireguard-ui/install.tar.gz
+        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.6.2/wireguard-ui-v0.6.2-linux-amd64.tar.gz  -O /opt/wireguard-ui/install.tar.gz
         elif [[ $arch == i*86 ]]; then
-        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.4.0/wireguard-ui-v0.4.0-linux-386.tar.gz  -O /opt/wireguard-ui/install.tar.gz
+        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.6.2/wireguard-ui-v0.6.2-linux-386.tar.gz  -O /opt/wireguard-ui/install.tar.gz
         elif  [[ $arch == arm* ]]; then
-        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.4.0/wireguard-ui-v0.4.0-linux-arm.tar.gz  -O /opt/wireguard-ui/install.tar.gz
+        wget https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.6.2/wireguard-ui-v0.6.2-linux-arm.tar.gz  -O /opt/wireguard-ui/install.tar.gz
     fi
     wget https://raw.githubusercontent.com/daniel-hiller/Wireguard-AIO-installer/main/wgui_service/wgui.path  -O /etc/systemd/system/wgui.path
     if [ -f "/bin/systemctl" ]; then
